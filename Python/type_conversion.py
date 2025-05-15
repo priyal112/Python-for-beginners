af17beb9e024e7ebcad374a69064baca4d8ea632
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-05-15 21:05:53 +0530

    first message

---
+++ Python/type_conversion.py
@@ -0,0 +1,26 @@
+#type conversion
+
+value = input("Enter your value: ")
+print(value)
+print(type(value))
+
+#int - convert value to int
+n1 = int(value)
+print(n1)
+print(type(n1))
+print(n1+5)
+
+#float - convert value to float
+f1 = float(value)
+print(f1)
+print(type(f1))
+print(f1+5)
+
+
+#str - convert value to str
+i = 2
+s = str(i)
+print(type(i))
+print(type(s))
+print("length: ",len(s))
+
---
+++ Python/type_conversion2.py
@@ -0,0 +1,17 @@
+# list converstion
+
+v = input("Enter your name: ")
+print(v)
+list1 = list(v)
+print(list1)
+print(type(list1))
+
+#tuple
+t1 = tuple(v)
+print(t1)
+print(type(t1))
+
+#set
+set1 = set(v)
+print(set1)
+print(type(set1))
