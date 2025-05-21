e36ee5357da36d17b97ac5aeec0c3ef2c485a232
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-05-21 21:03:33 +0530

    first class

--- Python/Map_&_Filter_Reduce_function/Reduce_function.py
+++ Python/Map_&_Filter_Reduce_function/Reduce_function.py
@@ -1,1 +1,21 @@
-# 
+# Reduce Function
+
+import functools
+
+def sum(x, y):
+    return x + y
+
+mylist = [34, 5, 6, 6]
+
+ans = functools.reduce(sum, mylist)
+print(ans)
+
+#minimum
+
+min = functools.reduce(lambda x,y : x if x<y else y, mylist)
+print(min)
+
+#Maximum
+
+min = functools.reduce(lambda x,y : x if x>y else y, mylist)
+print(min)
