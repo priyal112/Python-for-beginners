a5576a7c13fb94a14ec3b9a3e43c5e9932be0531
Priyal Shrivastava <priyalshrivastava006@gmail.com>
2025-05-22 20:49:45 +0530

    first class

--- Python/I_o_operations/read_example.py
+++ Python/I_o_operations/read_example.py
@@ -1,3 +1,19 @@
 # open
 file = open("abc.txt", "r")
 
+#read
+
+#ans = file.read(2)  #2 character
+#print(ans)
+
+#result = file.readline()  # one line
+#print(result)
+
+answer = file.readlines()  # multiple lines
+#print(answer)
+
+for l in answer:
+    print(l, end="")
+
+#close
+file.close()
